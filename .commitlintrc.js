e7089967b487cc90d65820d52bbad74ccf597c9c
Daniel Oliveira <danielso2007@gmail.com>
2025-11-22 21:41:02 -0300

    feat: Adicionado o husky no proejto.

---
+++ .commitlintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [2, 'always', ['sentence-case', 'lower-case']],
+    'subject-full-stop': [0]
+  }
+};
